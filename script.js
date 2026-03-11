63560535d6f1932fbed1a2c8cb77515c5d786acb
chaitnyaburkul <135574610+chaitnyaburkul@users.noreply.github.com>
2026-03-11 22:38:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateForm(){
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+
+if(name=="" || email=="")
+{
+alert("Please fill all fields");
+return false;
+}
+
+alert("Message sent successfully!");
+return true;
+
+}
